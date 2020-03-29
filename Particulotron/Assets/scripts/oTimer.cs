c452a865ef7b1a7a6488dc2894c8a8383689b81b
molasgregoire <44722763+molasgregoire@users.noreply.github.com>
2020-03-29 22:18:08 +0200

    new

---
+++ Particulotron/Assets/scripts/oTimer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oTimer : MonoBehaviour
+{
+    public float tps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //petit timer des familles
+    void OnGUI()
+    {
+        tps += Time.deltaTime * 0.5f;
+
+        float minutes = Mathf.Floor(tps / 60);
+        float seconds = tps % 60;
+
+        GUI.Label(new Rect(15, 15, 2500, 1000), minutes + ":" + Mathf.RoundToInt(seconds));
+    }
+}
