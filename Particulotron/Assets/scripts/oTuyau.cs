052c10191085484abc14b3fb5836d82056b67a31
molasgregoire <44722763+molasgregoire@users.noreply.github.com>
2020-03-30 21:34:01 +0200

    bricolage

--- Particulotron/Assets/scripts/oTuyau.cs
+++ Particulotron/Assets/scripts/oTuyau.cs
@@ -7,26 +7,29 @@ public class oTuyau : MonoBehaviour
     public GameObject backCircle;
     public GameObject frontCircle;
     public GameObject moveCircle;
+    //public GameObject neonTest;
 
     public List<GameObject> circles;
-    public int nbCircles = 5;
+    public int nbCircles = 11;
 
     // Start is called before the first frame update
     void Start()
     {
         
-        
+        /*
         backCircle = new GameObject();
         backCircle.AddComponent<SpriteRenderer>();
         backCircle.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("neonCircle") as Sprite;
         float ratioC = 1 / backCircle.GetComponent<SpriteRenderer>().size.x;
         
         backCircle.transform.localScale = new Vector3(0.1f, 0.1f, 0);
+        backCircle.GetComponent<SpriteRenderer>().color = new Color(209, 0, 250, 200);
 
         frontCircle = new GameObject();
         frontCircle.AddComponent<SpriteRenderer>();
         frontCircle.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("neonCircle") as Sprite;
         frontCircle.transform.localScale = new Vector3(1.1f, 1.1f, 0);
+        frontCircle.GetComponent<SpriteRenderer>().color = new Color(209, 0, 250, 200);
         /*
         moveCircle = new GameObject();
         moveCircle.AddComponent<SpriteRenderer>();
@@ -41,11 +44,16 @@ public class oTuyau : MonoBehaviour
             tmp.AddComponent<SpriteRenderer>();
             tmp.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("neonCircle") as Sprite;
             tmp.transform.localScale = new Vector3(i*dist,i*dist , 0);
-            tmp.transform.position = new Vector3(0, 0, -1);
+            tmp.transform.position = new Vector3(0, 0, 0.5f);
+            tmp.GetComponent<SpriteRenderer>().color = new Color(209, 0, 250, 0.8f);
             circles.Add(tmp);
         }
-
-       
+        /*
+        neonTest = new GameObject();
+        neonTest.AddComponent<SpriteRenderer>();
+        neonTest.GetComponent<SpriteRenderer>().sprite = Resources.Load<Sprite>("neonLine") as Sprite;
+        neonTest.transform.localScale = new Vector3(0.45f, 0.5f, 1);
+        neonTest.transform.position = new Vector3(0, 0, 1);*/
     }
 
     // Update is called once per frame
@@ -55,6 +63,8 @@ public class oTuyau : MonoBehaviour
         for (int i = 0; i < nbCircles; i++)
         {
             growAndBack(circles[i]);
+
+            transparency(circles[i]);
         }
     }
 
@@ -62,11 +72,18 @@ public class oTuyau : MonoBehaviour
     {
         if (subject.transform.localScale.x < 1.1f)
         {
-            subject.transform.localScale += new Vector3(0.003f, 0.003f, 0);
+            subject.transform.localScale += new Vector3(0.01f, 0.01f, 0);
         }
         else
         {
             subject.transform.localScale = new Vector3(0.1f, 0.1f, 0);
         }
+    }
+
+    void transparency(GameObject subject)
+    {
+        float size = subject.transform.localScale.x;
+        float trans = ( size) / 1.1f;
+        subject.GetComponent<SpriteRenderer>().color = new Color(209, 0, 250, trans);
     }
 }
