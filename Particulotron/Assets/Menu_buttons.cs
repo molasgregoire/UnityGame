b48d9cdcee3f5644046d613f8deffdf5767b9c35
Alexis <alexis.p.cohen@gmail.com>
2020-05-05 13:33:22 +0200

    Added main menu scene with some buttons

---
+++ Particulotron/Assets/Menu_buttons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+
+public class Menu_buttons : MonoBehaviour
+{
+
+    public void PlayGame()
+    {
+       UnityEngine.Debug.Log("Play_button pressed");
+       SceneManager.LoadScene(1);
+    }
+
+    public void OpenCrafting()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
