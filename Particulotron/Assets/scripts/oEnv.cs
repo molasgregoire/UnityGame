45c3941c5a365a28a19a7d9e2db56c919a286be7
Lifshitz <fanny.eustachon@hotmail.fr>
2020-05-19 21:52:27 +0200

    Score+Button not working

--- Particulotron/Assets/scripts/oEnv.cs
+++ Particulotron/Assets/scripts/oEnv.cs
@@ -17,7 +17,8 @@ public class oEnv : MonoBehaviour
     public oAimant Aimant;
 
     public GameObject Main;
-    public GameObject Score;
+    public GameObject Score; //Pour le panneau de score
+    public GameObject Scoretext;
 
     public oObstacle test;
     public List<oObstacle> listObs = new List<oObstacle>();
@@ -54,6 +55,7 @@ public class oEnv : MonoBehaviour
         Aimant = Main.AddComponent<oAimant>();
         Aimant.Part = Particule;
         chronoTarget = startTime;
+        Scoretext.SetActive(false);
 
         //pose des obstacles
         // >> pour linstant manuel, mais à initialiser depuis le createur de niveau (?)
@@ -109,6 +111,7 @@ public class oEnv : MonoBehaviour
         //Affichage du score
         if(oTimer.tps > maxTime+Time.deltaTime && oTimer.tps < maxTime+2*Time.deltaTime) {
           affichageScore();
+          Scoretext.SetActive(true);
         }
     }
 
@@ -291,16 +294,7 @@ public class oEnv : MonoBehaviour
       Score.GetComponent<SpriteRenderer>().color = new Color(1f,1f,1f,0.7f);
       Score.GetComponent<SpriteRenderer>().sortingOrder = 2;
 
-      //Score.AddComponent<Canvas>();
-      //Score.GetComponent<Canvas>().renderMode = RenderMode.ScreenSpaceOverlay;
-      //Score.AddComponent<Text>();
-      //Text CompText = Score.GetComponent<Text>();
-      //CompText.fontSize = 30;
-      //CompText.color = Color.white;
-      //Score.GetComponent<RectTransform>().sizeDelta = new Vector2(CompText.fontSize * 10, 100);
-      //CompText.text = "Score : " + score.ToString();
-      //Score.GetComponent<Meshrenderer>().enabled = false;
-
+      Scoretext.GetComponentInChildren<Text>().text = "Score\n" + score.ToString();
     }
 
     public void designWow()
