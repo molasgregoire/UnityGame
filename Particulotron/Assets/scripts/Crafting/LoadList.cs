4b826447a7fb086e03d85987177011bdf1a870df
Alexis <alexis.p.cohen@gmail.com>
2020-05-24 20:01:56 +0200

    Added Load Menu

---
+++ Particulotron/Assets/scripts/Crafting/LoadList.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadList : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject buttonTemplate;
+
+   
+    public void GenButton(Baryon baryon,List<ElmParticule> quarks)
+    {
+
+
+
+            GameObject button = Instantiate(buttonTemplate) as GameObject;
+            button.SetActive(true);
+
+            button.GetComponent<ButtonListButton>().SetBaryonQuarks(baryon, quarks);
+
+            button.transform.SetParent(buttonTemplate.transform.parent, false);
+        
+        
+    }
+}
